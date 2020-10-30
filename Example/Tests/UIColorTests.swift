1b33c91bf7ca5137ffb4742cf4d5bb9f0211e316
史彦超 <daviondk@163.com>
2020-10-30 18:55:48 +0800

    UIColor

---
+++ Example/Tests/UIColorTests.swift
@@ -0,0 +1,46 @@
+//
+//  UIColorTests.swift
+//  SycKit_Tests
+//
+//  Created by swift-syc on 2020/10/30.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+import XCTest
+import SycKit
+
+class UIColorTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // 靛青    #4B0082    75,0,130
+        // 16 进制
+        let color1 = UIColor.yc.getColor(hexString: "#4B0082", alpha: 1)
+        // 10 进制
+        let color2 = UIColor.yc.getColor(hexInt: 4915330, alpha: 1)
+        // rgb
+        let color3 = UIColor.yc.getColor(r: 75, g: 0, b: 130, a: 1)
+        print("color1 ==== \(color1)")
+        print("color2 ==== \(color2)")
+        print("color3 ==== \(color3)")
+        assert(color1 == color2, "靛青    #4B0082    75,0,130")
+        assert(color2 == color3, "靛青    #4B0082    75,0,130")
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
--- SycKit/Classes/UIColor+Syc.swift
+++ SycKit/Classes/UIColor+Syc.swift
@@ -9,5 +9,34 @@
 import UIKit
 
 extension SycStruct where Base: UIColor {
-
+    /// RGB
+    public static func getColor(r: CGFloat, g: CGFloat, b: CGFloat, a: CGFloat = 1) -> UIColor {
+        return UIColor(red: r / 255, green: g / 255, blue: b / 255, alpha: a)
+    }
+    
+    /// 十六进制字符串 转换 UIcolor
+    public static func getColor(hexString: String, alpha: CGFloat = 1) -> UIColor {
+        let hexColor = hexString.trimmingCharacters(in: .whitespacesAndNewlines).replacingOccurrences(of: "#", with: "")
+        let scanner = Scanner(string: hexColor)
+        // 存储转换后的数值
+        var color: UInt64 = 0
+        scanner.scanHexInt64(&color)
+        let mask = 0x000000FF
+        let r = Int(color >> 16) & mask
+        let g = Int(color >> 8) & mask
+        let b = Int(color) & mask
+        let red   = CGFloat(r) / 255.0
+        let green = CGFloat(g) / 255.0
+        let blue  = CGFloat(b) / 255.0
+        return UIColor.init(red: red, green: green, blue: blue, alpha: alpha)
+    }
+    
+    /// Int 转换 UIcolor
+    public static func getColor(hexInt: Int, alpha: CGFloat = 1) -> UIColor {
+        // 转换 16 进制, 并位移
+        let red = CGFloat(((hexInt & 0xFF0000) >> 16)) / CGFloat(255.0)
+        let green = CGFloat(((hexInt & 0xFF00) >> 8)) / CGFloat(255.0)
+        let blue = CGFloat(hexInt & 0xFF) / CGFloat(255.0)
+        return UIColor.init(red: red, green: green, blue: blue, alpha: alpha)
+    }
 }
